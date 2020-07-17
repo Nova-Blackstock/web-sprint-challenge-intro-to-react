6393b8b0fe3be632cf17f827837ba86ccc519876
Nova Blackstock <fractalrefletions@gmail.com>
2020-07-17 13:44:45 -0700

    What about the droid attack on the wookiees?

--- starwars/src/components/BestCharacter.js
+++ starwars/src/components/BestCharacter.js
@@ -10,13 +10,43 @@ const kfBest = keyframes`
 const StyledBestCharacter = style.div`
     color: cyan;
     animation: ${kfBest} 5s ease-in-out forwards;
+    display: flex;
+    h1 {
+        padding: 50px;
+    }
+    .dexter{
+        color: red;
+        border-radius: 10px;
+        &:hover {
+            box-shadow: 0px 0px 50px 20px;
+        }
+    }
+    .iggy {
+        width: 21%;
+        color: green;
+        border-radius: 10px;
+        &:hover {
+            box-shadow: 0px 0px 50px 40px;
+        }
+    }
+    .crumb {
+        width: 29%;
+        color: red;
+        border-radius: 10px;
+        &:hover {
+            box-shadow: 25px 0px 50px 20px;
+        }
+    }
 `
 
 export default function BestCharacter(){
 
     return(
-        <div>
-            <StyledBestCharacter>Who is the hottest Star Wars character?</StyledBestCharacter>
-        </div>
+            <StyledBestCharacter>
+                <h1>Who's the hottest Star Wars character?</h1>
+                <img className= 'dexter' src='https://i.imgur.com/X87OFmZ.gif' alt='imagine pure aesthetic perfection'></img>
+                <img className= 'iggy' src='https://media.giphy.com/media/zSX7tUskUtMWs/giphy.gif' alt='imagine robot sex'></img>
+                <img className= 'crumb' src='https://66.media.tumblr.com/5fca4aaf1bb71fe62644323e6a4ef353/tumblr_pqxo242AOx1tdkro1o1_400.gifv' alt= 'imagine joy itself'></img>
+            </StyledBestCharacter>
     )
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,38 +2,50 @@
 import React from 'react'
 import style, {keyframes} from 'styled-components'
 
-const kfCharacter = keyframes`
-    100% {
-        background: cyan
-        color: maroon
-    }
-`
-
 const StyledCharacter = style.div`
     background: black;
     color: purple;
     box-shadow: -5px -5px 50px 10px;
     border-radius: 10px;
     box-shadow: -5px -5px 50px 10px;
-    animation: ${kfCharacter} 15s ease-in-out forwards;
+    margin: 2%;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    li {
+        &:hover {
+            background: orange;
+            color: white;
+            padding: 5px;
+            border-radius: 10px;
+            text-color: green;
+        }
+    }   
 `
 
 function Character(props) {
     console.log(props)
-    charactersData.map(character => {
+    const { charactersData } = props;
+    
         return (
             <StyledCharacter>
-                <h1>General {character.name}! You are a bold one.</h1>
+                {
+                charactersData.map(character => {
+                    return(
+                <div>    
+                {character.name === 'Obi-Wan Kenobi' ? <h1>General Ewan McGreggor! You are a bold one.</h1> : <h1>General {character.name}! You are an old one.</h1>}
                 <ul>
-                    <li>Identifies as {character.gender}</li>
-                    <li>Motto: when you're {character.height} and {character.hair_color}, dating is {character.gender === 'male' ? 'hard.' : 'fun!'} </li>
+                    {character.gender === 'n/a' ? <li>Identifies as non-binary.</li> : <li>Identifies as {character.gender}</li>}
+                    <li>Motto: when you're {character.height}" and {character.hair_color === 'none' | character.hair_color === 'n/a' ? 'bald' : character.hair_color}, dating is {character.gender === 'male' ? 'hard.' : 'fun!'} </li>
                     <li>Born {character.birth_year}</li>
                     <li>{character.name === 'Palpatine' ? 'I AM the Senate.' : 'I am not the Senate.'} </li>
                     <li>{character.name === 'Darth Vader' ? 'I hate sand.' : "Sand is ok."} </li>
                 </ul>
+                </div>)})
+                }
             </StyledCharacter>
-        )
-    }) 
-}
+            )
+    
+        }
 
 export default Character
